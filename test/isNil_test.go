35b0d0bfd42f392ecec131e60cab894eb272f3ed
tarrball <andrew.tarr@hotmail.com>
2017-12-31 15:47:54 -0600

    unreleated test file cleanup but i'm putting it in this branch anyway

--- test/isNil_test.go
+++ test/isNil_test.go
@@ -3,17 +3,17 @@ package test
 import "testing"
 import "github.com/tarrball/gophertest/mocks"
 
-func TestIsNilPassesIfNil(t *testing.T) {
-	sut := SUT{testContext: t, actual: nil}
-
-	sut.IsNil()
-}
-
 func TestIsNilFailsIfNotNil(t *testing.T) {
 	mock := mocks.GetMock(t)
 	mock.EXPECT().Errorf("Expected value was not nil.")
 
 	sut := SUT{testContext: mock, actual: "non nil value"}
+
+	sut.IsNil()
+}
+
+func TestIsNilPassesIfNil(t *testing.T) {
+	sut := SUT{testContext: t, actual: nil}
 
 	sut.IsNil()
 }
