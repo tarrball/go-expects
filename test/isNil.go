0ffb654e62e36ac064db9fe7799c0791d3789c78
tarrball <andrew.tarr@hotmail.com>
2017-12-31 15:42:16 -0600

    added tests for nil/not nil checks

--- test/isNil.go
+++ test/isNil.go
@@ -2,7 +2,7 @@ package test
 
 // IsNil expects the system under test value to be nil.
 func (sut SUT) IsNil() {
-	if sut.actual == nil {
+	if sut.actual != nil {
 		sut.testContext.Errorf("Expected value was not nil.")
 	}
 }
--- test/isNotNil_test.go
+++ test/isNotNil_test.go
@@ -1,1 +1,19 @@
 package test
+
+import "testing"
+import "github.com/tarrball/gophertest/mocks"
+
+func TestIsNotNilFailsIfNil(t *testing.T) {
+	mock := mocks.GetMock(t)
+	mock.EXPECT().Errorf("Expected value was nil.")
+
+	sut := SUT{testContext: mock, actual: nil}
+
+	sut.IsNotNil()
+}
+
+func TestIsNotNilPassesIfNotNil(t *testing.T) {
+	sut := SUT{testContext: t, actual: "non nil value"}
+
+	sut.IsNotNil()
+}
